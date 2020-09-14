bdc17403c27b0d868a4d96d22fa45116b7d5bd65
Timo Kock <timo.kock@movicoders.com>
2020-09-14 11:58:28 +0200

    Implemented Edit User Action

--- OnSale.Web/Helpers/IUserHelper.cs
+++ OnSale.Web/Helpers/IUserHelper.cs
@@ -26,5 +26,11 @@ namespace OnSale.Web.Helpers
         Task<SignInResult> ValidatePasswordAsync(User user, string password);
         
         Task<User> AddUserAsync(AddUserViewModel model, Guid imageId, UserType userType);
+        
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<User> GetUserAsync(Guid userId);
     }
 }
